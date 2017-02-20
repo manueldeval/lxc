a8b07e5c9779ed929273d8eb5453db936c2d7d3b
manueldeval <manuel.deval@gmail.com>
2017-02-20 12:41:08 +0100

    Modification du setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-SERVICE_IP=192.168.0.26
+SERVICE_IP=$(ip addr show ens192 | grep "inet " | sed "s/ *inet//" | sed "s/\/26.*//" | xargs)
+ADMIN_IP=$(ip addr show ens160 | grep "inet " | sed "s/ *inet//" | sed "s/\/26.*//" | xargs)
 yum install xinetd -y
 yum install epel-release -y
 yum install debootstrap perl libvirt -y
@@ -14,10 +15,11 @@ systemctl start libvirtd
 
 # Listen only on main ip!
 echo "ListenAddress $SERVICE_IP" >> /etc/ssh/sshd_config
+echo "ListenAddress $ADMIN_IP"   >> /etc/ssh/sshd_config
 service sshd restart
 
 # Server.py
 
-firewall-cmd --zone=public --add-port=8080/tcp
-yum install python-bottle -y 
-yum install python-yaml -y 
+#firewall-cmd --zone=public --add-port=8080/tcp
+#yum install python-bottle -y 
+#yum install python-yaml -y 
